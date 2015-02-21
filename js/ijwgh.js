72978798bf9cb7fd530474d09b987fbb71b5f11a
Guilherme <setetres@gmail.com>
2015-02-21 19:08:52 -0200

    Fix

---
+++ js/ijwgh.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    // twitter
+
+    $('#tweet').twittie({
+        username: 'ap_oddities',
+        apiPath: 'http://file.setetres.st/v5/twitter/tweet.php',
+        dateFormat: '%b %d, %Y',
+        template: '{{tweet}} <div class="date">{{date}}</div>',
+        count: 20,
+        hideReplies: true
+    });
+
+});
