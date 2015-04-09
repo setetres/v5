3136054b67dbf756242c4e10620497f40c170984
Guilherme Pangnotta <setetres@gmail.com>
2015-04-09 12:08:09 -0300

    Fix Bucket

--- js/setetres.js
+++ js/setetres.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
     $('#tweet').twittie({
         username: 'setetres',
-        apiPath: 'http://file.setetres.st/v5/twitter/tweet.php',
+        apiPath: 'http://files.setetres.st/v5/twitter/tweet.php',
         dateFormat: '%b %d, %Y',
         template: '{{tweet}} <div class="date">{{date}}</div>',
         count: 20,
